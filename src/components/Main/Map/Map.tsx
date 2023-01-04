39b915be5d3a8402235480d005cd3dcab4c387b3
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 15:12:44 +0400

    bug fix

--- src/components/Main/Map/Map.tsx
+++ src/components/Main/Map/Map.tsx
@@ -1,5 +1,28 @@
+import { useAppSelector } from 'hooks';
 import React from 'react';
+import styled from 'styled-components';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { Route } from './components';
+import 'leaflet/dist/leaflet.css';
 
 export const Map = () => {
-  return <div>Map</div>;
+  const coord = useAppSelector((state) => state.orders.startCoord);
+
+  return (
+    <MapContainerStyled center={coord} zoom={10} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Route />
+    </MapContainerStyled>
+  );
 };
+
+const MapContainerStyled = styled(MapContainer)`
+  &.leaflet-container {
+    width: 50vw;
+    height: 90vh;
+  }
+`;
