117d96a4cb208ce398efa0b9511fd8ee1fdbbd68
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 13:31:47 +0400

    added Main router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,8 @@
+import { Main } from 'pages';
 import React from 'react';
 
+// If the project develops, all routes will be described here.
+
 export const AppRouter = () => {
-  return <div>Список заказов</div>;
+  return <Main />;
 };
