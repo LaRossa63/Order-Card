906866cdcbf96ccb938bb3c76b110d32ec809024
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 15:03:11 +0400

    init map

--- src/components/Main/Map/Map.tsx
+++ src/components/Main/Map/Map.tsx
@@ -1,27 +1,5 @@
-import { useAppSelector } from 'hooks';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import styled from 'styled-components';
-import { Route } from './components';
-import 'leaflet/dist/leaflet.css';
+import React from 'react';
 
 export const Map = () => {
-  const coord = useAppSelector((state) => state.orders.startCoord);
-
-  return (
-    <MapContainerStyled center={coord} zoom={10} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      <Route />
-    </MapContainerStyled>
-  );
+  return <div>Map</div>;
 };
-
-const MapContainerStyled = styled(MapContainer)`
-  &.leaflet-container {
-    width: 50vw;
-    height: 90vh;
-  }
-`;
