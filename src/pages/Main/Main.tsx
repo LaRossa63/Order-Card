fd21450100411d0620a9ac1de4ce7fa5e470d16c
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 13:33:56 +0400

    makes structure for router Main

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,29 @@
+import { Footer, Header, Map, Orders } from 'components/Main';
+import React from 'react';
+import styled from 'styled-components';
+
+export const Main = () => {
+  return (
+    <>
+      <Header />
+
+      <ContainerWrapper>
+        <Container>
+          <Orders />
+          <Map />
+        </Container>
+      </ContainerWrapper>
+
+      <Footer />
+    </>
+  );
+};
+
+const ContainerWrapper = styled.div`
+  padding: 0 15px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
