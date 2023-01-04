86ea24329c8d9bc986966eb75ce7c2cfe976cb2c
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 16:46:32 +0400

    added Header

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import styled from 'styled-components';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Text>Список заказов</Text>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 20px;
+
+  box-shadow: 0px 1px 5px #fcfcfd;
+  background-color: #eeeef7;
+  border-radius: 0 0 12px 12px;
+`;
+
+const Text = styled.h3`
+  font-size: 24px;
+`;
