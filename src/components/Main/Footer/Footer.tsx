69ed4a30bd95158da8f0344b75c5837e983310c5
Maxim Sevostyanov <maksimsevostuanow@mail.ru>
2023-01-04 16:46:24 +0400

    added Footer

--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Text>
+        Тестовое задание выполнил для
+        <Link href="https://intelogis.ru/"> Intelogis</Link>
+      </Text>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 20px;
+
+  box-shadow: 0px 1px 5px #fcfcfd;
+  background-color: #eeeef7;
+  border-radius: 12px 12px 0 0;
+`;
+
+const Text = styled.h3``;
+
+const Link = styled.a``;
